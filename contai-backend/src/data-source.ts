06d3bebfe526905376ecd306b4ed6cab917b43d4
sno0s <maathdesigner@outlook.com>
2025-06-03 15:22:47 -0300

    docker adicionado

--- contai-backend/src/data-source.ts
+++ contai-backend/src/data-source.ts
@@ -4,11 +4,11 @@ import { Launch } from './entity/Launch';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432, //porta do banco de dados
-  username: 'postgres',
-  password: '2304012232',
-  database: 'ContAI',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || 'postgres',
+  database: process.env.DB_NAME || 'contai_db',
   synchronize: true,
   logging: false,
   entities: [Launch],
